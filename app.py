d43832185464afc2fac9086e3d3f7abe27f7832a
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-26 17:51:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,45 @@
-from PIL import Image
-import requests
+# Important Plugins
+import streamlit as st #start off by loading streamlit
+import nltk
+nltk.download('punkt')
 
-url = "https://www.horoscope.com/images-US/signs/profile-aries.png"
-im_aries = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-taurus.png"
-im_taurus = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-gemini.png"
-im_gemini = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-cancer.png"
-im_cancer = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-leo.png"
-im_leo = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-virgo.png"
-im_virgo = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-libra.png"
-im_libra = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-scorpio.png"
-im_scorpio = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-sagittarius.png"
-im_sagittarius = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-capricorn.png"
-im_capricorn = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-aquarius.png"
-im_aquarius = Image.open(requests.get(url, stream=True).raw)
-url = "https://www.horoscope.com/images-US/signs/profile-pisces.png"
-im_pisces = Image.open(requests.get(url, stream=True).raw)
-image_name = []
-image_name.append({'name':'Aries', 'image':im_aries, 'date': 'Mar 21-Apr 20'})
-image_name.append({'name':'Taurus', 'image':im_taurus, 'date': 'Apr 21-May 20'})
-image_name.append({'name':'Gemini', 'image':im_gemini, 'date': 'May 21-Jun 21'})
-image_name.append({'name':'Cancer', 'image':im_cancer, 'date': 'Jun 22-Jul 22'})
-image_name.append({'name':'Leo', 'image':im_leo, 'date': 'Jul 23-Aug 22'})
-image_name.append({'name':'Virgo', 'image':im_virgo, 'date': 'Aug 23-Sep 22'})
-image_name.append({'name':'Libra', 'image':im_libra, 'date': 'Sep 23-Oct 22'})
-image_name.append({'name':'Scorpio', 'image':im_scorpio, 'date': 'Oct 23-Nov 22'})
-image_name.append({'name':'Sagittarius', 'image':im_sagittarius, 'date': 'Nov 23-Dec 21'})
-image_name.append({'name':'Capricorn', 'image':im_capricorn, 'date': 'Dec 22-Jan 19'})
-image_name.append({'name':'Aquarius', 'image':im_aquarius, 'date': 'Jan 20-Feb 18'})
-image_name.append({'name':'Pisces', 'image':im_pisces, 'date': 'Feb 19-Mar 20'})
-st.write("hola")
+# importing Newspaper3k
+import newspaper
+from newspaper import Article
+
+# Custom Functions
+#---------------------------------
+#article downloader
+def run_api(user_url_input):
+    article = Article(user_url_input)
+    article.download()
+    article.parse()
+    article.nlp()
+    return meta_extraction(article)
+
+#article meta extraction + summarizer
+def meta_extraction(article):
+    st.write('**The title of this article is ** ' + article.title)
+    st.write('**The author of this article is **' + str(article.authors))
+    st.write('**The published date of this article is **' + str(article.publish_date))
+    st.write('**The source of this article is **' + article.source_url)
+
+    st.write('## Top Keywords of this Article Below:')
+    st.write(str(article.keywords))
+    st.write('## Summary of Article Below:')
+    st.write(article.summary)
+    return
+
+
+# Page Design
+#-------------------------------
+st.markdown('# This Streamlit application will scrape, display some meta data, and summarize articles.')
+
+# Text Input
+user_url_input = st.text_input("Please enter the URL of article")
+
+# Button to Run the script
+if st.button('Summarize the Article'):
+    st.write(run_api(user_url_input))
+else:
+    st.write(st.markdown('**Button not yet executed.**'))
